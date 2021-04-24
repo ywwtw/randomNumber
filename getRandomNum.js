4340c9c2f86863f90454eef2ca0a633de96e1fbe
yww <yww85925@gmail.com>
2021-04-24 20:08:26 +0800

    fix 略過空號

--- getRandomNum.js
+++ getRandomNum.js
@@ -1,34 +1,130 @@
 const myclassList=[
     {
-        id:"1",
-        img:"https://cdn2.ettoday.net/images/5037/5037459.jpg"
+        id:1,
+        img:"./img/1.jpg"
     },
     {
-        id:"2",
-        img:"https://yt3.ggpht.com/ytc/AAUvwngno5cJVjzt7xzX-xjkansV7YIcrxD5G34FXCHIFQ=s900-c-k-c0x00ffffff-no-rj"},
+        id:2,
+        img:"./img/2.jpg"},
     {
-        id:"3",
-        img:"https://upload.wikimedia.org/wikipedia/zh/d/d0/JaketheDog.png"
+        id:3,
+        img:"./img/3.jpg"
     },
     {
-        id:"4",
-        img:"https://stickershop.line-scdn.net/stickershop/v1/product/1627/LINEStorePC/main.png;compress=true"
+        id:4,
+        img:"./img/4.jpg"
     },
     {
-        id:"5",
-        img:"https://cdn.shopify.com/s/files/1/0264/9063/4337/files/PAW_GTG_FW20_F_Chase_001_flop_CGI_x1500.png?v=1592931209"
+        id:5,
+        img:"./img/5.jpg"
     },
     {
-        id:"6",
-        img:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS20mNitxRqquo4omgXmd6wP4i_LszokoiZSg&usqp=CAU"
+        id:6,
+        img:"./img/6.jpg"
     },
     {
-        id:"7",
-        img:"https://upload.wikimedia.org/wikipedia/zh/thumb/1/18/%E8%A5%BF%E7%B4%A2.jpg/250px-%E8%A5%BF%E7%B4%A2.jpg"
+        id:7,
+        img:"./img/7.jpg"
     },
     {
-        id:"8",
-        img:"https://goodsmileshop.com/medias/sys_master/images/images/h72/hab/9156389797918.jpg"
+        id:8,
+        img:"./img/88.jpg"
+    },
+    {
+        id:9,
+        img:"./img/9.jpg"
+    },
+    {
+        id:10,
+        img:"./img/10.jpg"
+    },
+    {
+        id:11,
+        img:"./img/11.jpg"
+    },
+    {
+        id:12,
+        img:"./img/12.jpg"
+    },
+    {
+        id:13,
+        img:"./img/13.jpg"
+    },
+    {
+        id:14,
+        img:"./img/14.jpg"
+    },
+    {
+        id:15,
+        img:"./img/15.jpg"
+    },
+    {
+        id:16,
+        img:"./img/16.jpg"
+    },
+    {
+        id:31,
+        img:"./img/31.jpg"
+    },
+    {
+        id:32,
+        img:"./img/32.jpg"
+    },
+    {
+        id:33,
+        img:"./img/33.jpg"
+    },
+    {
+        id:34,
+        img:"./img/34.jpg"
+    },
+    {
+        id:35,
+        img:"./img/35.jpg"
+    },
+    {
+        id:36,
+        img:"./img/36.jpg"
+    },
+    {
+        id:37,
+        img:"./img/37.jpg"
+    },
+    {
+        id:38,
+        img:"./img/38.jpg"
+    },
+    {
+        id:39,
+        img:"./img/39.jpg"
+    },
+    {
+        id:40,
+        img:"./img/40.jpg"
+    },
+    {
+        id:41,
+        img:"./img/41.jpg"
+    },
+    {
+        id:42,
+        img:"./img/42.jpg"
+    },
+    {
+        id:43,
+        img:"./img/43.jpg"
+    },
+    {
+        id:44,
+        img:"./img/44.jpg"
+    },
+    {
+        id:45,
+        img:"./img/45.jpg"
+    },
+    {
+        id:46,
+        img:"./img/46.jpg"
     },
 ]
 
@@ -48,16 +144,21 @@ myclassList.forEach(myclass => {
 });
 function start(){
     let result = document.getElementById('result');
-    let getRandom = setInterval(displayNum, 100);
-    function displayNum(){
-        const random = Math.floor(Math.random() * parseInt(8) + parseInt(1));   
-
-        result.innerHTML =`
-            <div><img src="${myclassList[random-1].img}" class="displayPic"></div>
+    let getRandom = setInterval(displayNum, 50);
+    function displayNum(){   
+        random = Math.floor(Math.random() * 46 + 1);
+        if(random>16 && random<31){
+            displayNum(); 
+        }else{
+            let picIndex = myclassList.findIndex(x=>x.id === random);
+            result.innerHTML =`
+            <div><img src="${myclassList[picIndex].img}" class="displayPic"></div>
             <div class="result">${random}號</div>
-            `
-    }
-    setTimeout(function(){
-        clearInterval(getRandom);
-    },3000);
+            `       
+        }                   
+            setTimeout(function(){
+                clearInterval(getRandom);
+            },3000);  
+                
+    }       
 }
